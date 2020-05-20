4b59b40b125344fca0c76d826498b844251a3ac8
emanabela <33659689+emanabela@users.noreply.github.com>
2020-05-20 15:23:33 +0200

    IdentityLoginRegister

---
+++ Nemesys/Nemesys/Areas/Identity/Data/NemesysUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Nemesys.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the NemesysUser class
+    public class NemesysUser : IdentityUser
+    {
+    }
+}
